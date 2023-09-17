ff5eb712e56957611d2bc163ddd6e5c54cdc4f47
itsmarsss <itsmarzzzzzz@gmail.com>
2023-09-17 02:03:36 -0400

    Add minor circular orbit

--- index.js
+++ index.js
@@ -60,7 +60,9 @@ async function loadQuestions() {
         if (element.question == "<ANSWER SLOT>" && element.answer == "<ANSWER SLOT>") {
             bmw_info.innerHTML += `
             <div class="real question answer">
-                <div class="accent" id="answer">(Hover/Click a Question)</div>
+                <div class="question_text">
+                    <div class="accent" id="answer">(Hover/Click a Question)</div>
+                </div>
             </div>`;
         } else {
             bmw_info.innerHTML += `
@@ -138,9 +140,9 @@ function updateAnswer(answer_text) {
     let angle = deg_interval * answer_index;
     let x_radius = Math.min(bmw_cont.offsetWidth, window.innerWidth) / 2 - 100;
     let y_radius = Math.min(bmw_cont.offsetHeight, window.innerHeight) / 2 - 100;
-    let x_trans = x_radius * Math.sin(degsToRads(angle)) - answer.parentElement.offsetWidth / 2;
-    let y_trans = -1 * y_radius * Math.cos(degsToRads(angle)) - answer.parentElement.offsetHeight / 2;
-    answer.parentElement.style.transform = `translateX(${x_trans}px) translateY(${y_trans}px)`;
+    let x_trans = x_radius * Math.sin(degsToRads(angle)) - answer.parentElement.parentElement.offsetWidth / 2;
+    let y_trans = -1 * y_radius * Math.cos(degsToRads(angle)) - answer.parentElement.parentElement.offsetHeight / 2;
+    answer.parentElement.parentElement.style.transform = `translateX(${x_trans}px) translateY(${y_trans}px)`;
 }
 
 function scrollToAnswer() {
